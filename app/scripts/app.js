de717bc82add34a252353a62fe71008da00512d0
Ben and Ann De Silva <benann@Bens-Mac-mini.local>
2015-08-19 23:22:07 -0700

    figureing out location

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,17 +8,26 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  .controller('MainCtrl', ['$scope', '$location', function($scope, $location) {
+    //TODO: Inject $location.path(url) to route instead of ng-router
+    $location.path('views/main.html');
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
+  // .config(['$routeProvider', function ($routeProvider) {
+  //   $routeProvider
+  //     .when('/', {
+  //       templateUrl: 'views/main.html',
+  //       controller: 'MainCtrl'
+  //     })
+  //     .when('/about', {
+  //       templateUrl: 'views/submitted.html',
+  //       controller: 'AboutCtrl'
+  //     })
+  //     .otherwise({
+  //       redirectTo: '/'
+  //     });
+  // }]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,6 @@
 angular.module('movietimeplusApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
+  .controller('MainCtrl', ['$scope', function($scope) {
+    //TODO: Inject $location.path(url) to route instead of ng-router
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
