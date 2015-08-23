e8019929aec8023dbb9f19972a551589e47c7321
Ben and Ann De Silva <benann@Bens-Mac-mini.local>
2015-08-22 17:26:55 -0700

    Main form almost complete UI design and structure.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,11 @@
 angular.module('movietimeplusApp')
   .controller('MainCtrl', ['$scope', function($scope) {
-    //TODO: Inject $location.path(url) to route instead of ng-router
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.email = '';
+    $scope.timeHourMin = '';
+    $scope.message ='';
+    $scope.workItems = [
+      {workType: 'Feature Item', workItem: 'Time working on visual effects for movie'},
+      {workType: 'Review Task', workItem: 'Time spent on reviewing the work of a junior artist'},
+      {workType: 'Other', workItem: ' Please state work item...'}
     ];
-    console.log('Im here');
   }]);
